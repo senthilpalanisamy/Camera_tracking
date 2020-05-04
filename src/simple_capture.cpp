8e284c9f9ade1e86be7f73f3b26886c26e6ca261
Senthil Palanisamy <senthillihtnes1994@gmailcom>
2020-05-03 22:58:28 -0500

    Added a class for frame grabber control

--- src/simple_capture.cpp
+++ src/simple_capture.cpp
@@ -10,44 +10,77 @@
 #include "opencv2/features2d.hpp"
 #include "opencv2/xfeatures2d.hpp"
 
-using namespace cv;
 using namespace cv::xfeatures2d;
 using std::cout;
 using std::endl;
+constexpr int HORIZONTAL_RES=2048;
+constexpr int VERTICAL_RES=2048;
+using cv::imshow;
+using cv::waitKey;
 
-int main(void)
+
+class frameGrabber
 {
-int horizontal_resolution = 2048;
-int vertical_resolution = 2048;
+  public:
+  cv::Mat image0, image1, image2, image3;
+  cv::Mat* image;
 
- //printf("started");
- cout<<"started\n";
+  //static uchar buf[HORIZONTAL_RES * VERTICAL_RES];
+  //uchar buf0[HORIZONTAL_RES * VERTICAL_RES];
+  //uchar buf1[HORIZONTAL_RES * VERTICAL_RES];
+  //uchar buf2[HORIZONTAL_RES * VERTICAL_RES];
+  //uchar buf3[HORIZONTAL_RES * VERTICAL_RES];
+  uchar *buf, *buf0, *buf1, *buf2, *buf3;
+  frameGrabber(const char* configPath)
+  {
+   cout<<"Enter Constructotr\n";
+   pxd_PIXCIopen("", "", configPath);
 
+   buf = (unsigned char*) malloc(  pxd_imageXdim()    // horizontal resolution
+                       * pxd_imageYdim()    // vertical resolution
+                       * sizeof(unsigned char));
+  
+   buf0 = (unsigned char*) malloc(  pxd_imageXdim()    // horizontal resolution
+                       * pxd_imageYdim()    // vertical resolution
+                       * sizeof(unsigned char));
+   
+   buf1 = (unsigned char*) malloc(  pxd_imageXdim()    // horizontal resolution
+                       * pxd_imageYdim()    // vertical resolution
+                       * sizeof(unsigned char));
+   buf2 = (unsigned char*) malloc(  pxd_imageXdim()    // horizontal resolution
+                       * pxd_imageYdim()    // vertical resolution
+                       * sizeof(unsigned char));
+   buf3 = (unsigned char*) malloc(  pxd_imageXdim()    // horizontal resolution
+                       * pxd_imageYdim()    // vertical resolution
+                       * sizeof(unsigned char));
+  }
 
-pxd_PIXCIopen("", "",  "./camera1.fmt");
-// pxd_goLive(0x1, // select PIXCI(R) unit 1
-//                    1);  // select frame buffer 1
-//
-pxd_doSnap(0x1, 1, 0);
+  void transferImagetoPC(size_t frameGrabberNo)
+  {
+    size_t cameraNo = 1 << frameGrabberNo;
+    pxd_doSnap(cameraNo, 1, 0);
 
-     unsigned char *buf = (unsigned char*) malloc(  pxd_imageXdim()    // horizontal resolution
-                        * pxd_imageYdim()    // vertical resolution
-                        * sizeof(short));
-   // auto buf = static_cast<uint8_t *>(pxd_imageXdim() * pxd_imageYdim()  * sizeof(short));
+    switch(frameGrabberNo)
+    {
+      case 0: buf = buf0;
+              image = &image0;
+              break;
 
+      case 1: buf = buf1;
+              image = &image1;
+              break;
 
-     // pxd_readushort(0x4,         // select PIXCI(R) unit 1
-     //                0,           // select frame buffer 1
-     //                0, 0,        // upper left X, Y AOI of buffer
-     //                -1, -1,      // lower right X, Y AOI of buffer,
-     //                             // -1 is an abbreviation for the maximum X or Y
-     //                buf,         // program buffer to be filled
-     //                pxd_imageXdim() * pxd_imageYdim(),
-     //                             // size of program buffer in short's
-     //                "Grey");     // color space to access
+      case 2: buf = buf2;
+              image = &image2;
+              break;
 
+      case 3: buf = buf3;
+              image = &image3;
+              break;
+      default: cout<<"Invalid number for frame grabber unit. Enter a number between 0-3";
+    }
 
-     pxd_readuchar(0x1,         // select PIXCI(R) unit 1
+    auto i = pxd_readuchar(cameraNo,         // select PIXCI(R) unit 1
                     1,           // select frame buffer 1
                     0, 0,        // upper left X, Y AOI of buffer
                     -1, -1,      // lower right X, Y AOI of buffer,
@@ -57,35 +90,43 @@ pxd_doSnap(0x1, 1, 0);
                                  // size of program buffer in short's
                     "Grey");     // color space to access
 
-    cv::Mat image = cv::Mat( pxd_imageYdim(),  pxd_imageXdim(), CV_8UC1, (unsigned char*)buf , 0 );
-    cout<< image;
+    *image = cv::Mat( pxd_imageYdim(),  pxd_imageXdim(), CV_8UC1, buf , 0 );
 
-    imshow("captured_image", image);
+  }
+
+  void transferAllImagestoPC()
+  {
+
+    for(size_t i=0; i < 4; i++)
+    {
+      transferImagetoPC(i);
+    }
+
+  }
+
+  void displayAllImages()
+  {
+    imshow("image0", image0);
+    imshow("image1", image1);
+    imshow("image2", image2);
+    imshow("image3", image3);
     waitKey(0);
+  }
 
+  ~frameGrabber()
+  {
+   pxd_PIXCIclose();
+   free(buf0);
+   free(buf1);
+   free(buf2);
+   free(buf3);
+  }
+};
 
-  //   pxd_readushort(0x1,         // select PIXCI(R) unit 1
-  //                  1,           // select frame buffer 1
-  //                  0, 0,        // upper left X, Y AOI of buffer
-  //                  -1, -1,      // lower right X, Y AOI of buffer,
-  //                               // -1 is an abbreviation for the maximum X or Y
-  //                  buf,         // program buffer to be filled
-  //                  pxd_imageXdim() * pxd_imageYdim(),
-  //                               // size of program buffer in short's
-  //                  "Grey");     // color space to access
-  //  image = cv::Mat( pxd_imageYdim(),  pxd_imageXdim(), CV_8UC1, buf , 0 );
-  //  cout<< image;
-
-  //  imshow("captured_image", image);
-  //  waitKey(0);
-
-    cout<<"finished\n";
-    //printf("finished")
-
-
-free(buf);
-pxd_PIXCIclose();
-
+int main(void)
+{
+  frameGrabber imageTransferObj("camera2.fmt");
+  imageTransferObj.transferAllImagestoPC();
+  imageTransferObj.displayAllImages();
 
 }
-
