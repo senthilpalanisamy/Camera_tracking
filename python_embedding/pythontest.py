3d6a70b112f04d56a9dacc41bdfcf73484447a90
Senthil Palanisamy <senthillihtnes1994@gmail.com>
2020-09-29 15:35:44 -0500

    Tested pybind11 for python embedding in C++

---
+++ python_embedding/pythontest.py
@@ -0,0 +1,3 @@
+def generate_list(num=5):
+    res = [i**2 for i in range(num)]
+    return res
---
+++ python_embedding/test.cpp
@@ -0,0 +1,37 @@
+#include <pybind11/embed.h>
+#include <iostream>
+#include <vector>
+#include <pybind11/stl.h>
+
+using std::cout;
+using std::vector;
+using std::list;
+
+namespace py = pybind11;
+
+
+int main()
+{
+  py::scoped_interpreter guard{}; // start the interpreter and keep it alive
+  //py::module sys = py::module::import("sys");
+  //py::print(sys.attr("path"));
+
+  py::module test = py::module::import("pythontest");
+  auto result = test.attr("generate_list")(5);
+  auto resultNew = py::cast<py::list>(result);
+  // auto resultNew = static_cast<<int> > result;
+
+  // auto resultNew = test.cast<list>();
+  cout<<"Printing numbers\n";
+  for(auto a: resultNew)
+  {
+    cout<<a<<"\t";
+  }
+  cout<<"\n";
+  cout<<"finish";
+
+
+
+
+}
+
