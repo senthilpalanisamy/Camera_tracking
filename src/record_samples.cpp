e525704b5e77f63bbeed0eb19eabc2def4ed6052
senthilpalanisamy <senthillihtnes1994@gmail.com>
2020-11-05 18:46:18 -0600

    Modified files for lossless video capture through images

--- src/record_samples.cpp
+++ src/record_samples.cpp
@@ -2,30 +2,27 @@
 #include "video_recorder.hpp"
 
 using std::to_string;
-using cv::VideoWriter;
 
 
 videoRecorder::videoRecorder(const int writerCount, const string baseName,
                 const Size imageSize, double fps,
-		const string ouputPath="./results");
-
+		const string outputPath)
 {
-  vector<VideoWriter> allWriters;
   string fileFormat=".h265";
 
   for(int i=0; i < writerCount; ++i)
   { 
     string filePath = outputPath + baseName + to_string(i) + fileFormat;
-    allWriters.emplace_back(filePath, CV_FOURCC('H', '2', '6', '5'),
+    allWriters.emplace_back(filePath, cv::VideoWriter::fourcc('H', '2', '6', '5'),
 		            fps, imageSize);
   }
 }
 
-videoRecorder::writeFrames(const<Mat> newFrames)
+void videoRecorder::writeFrames(const vector<Mat>& newFrames)
 {
   for(int i=0; i<allWriters.size(); i++)
   {
-    allWriters.write(newFrames[i]);
+    allWriters[i].write(newFrames[i]);
   }
 }
 
@@ -35,3 +32,32 @@ videoRecorder::~videoRecorder()
 }
 
 
+int main()
+{
+
+   frameGrabber imageTransferObj("./config/camera3.fmt");
+
+   imageTransferObj.transferAllImagestoPC();
+   auto image0 = imageTransferObj.image0;
+   auto recorder = videoRecorder(4, "sample_trial", image0.size(),
+		                 30);
+
+   for(int i=0; i<100; i++)
+   {
+
+
+    vector<Mat> images;
+
+    images.push_back(imageTransferObj.image0);
+    images.push_back(imageTransferObj.image1);
+    images.push_back(imageTransferObj.image2);
+    images.push_back(imageTransferObj.image3);
+	 
+    recorder.writeFrames(images);
+
+ 
+   }
+
+
+
+}
