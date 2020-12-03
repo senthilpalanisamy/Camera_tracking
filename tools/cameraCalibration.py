6c21999042c0cdf85df3cc6ccdc25f7e64450f4f
senthilpalanisamy <senthillihtnes1994@gmail.com>
2020-12-03 13:42:59 -0600

    Made changes to cmakelists

--- tools/cameraCalibration.py
+++ tools/cameraCalibration.py
@@ -2,7 +2,15 @@ import numpy as np
 import cv2 as cv
 import glob
 import json
+
+import os
+import sys
 # termination criteria
+
+output_base_path = './config/camera_intrinsics_1024x1024/'
+if(not os.path.isdir(output_base_path)):
+    sys.exit('please provide a correct path for writing results')
+
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 camera_no=0
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
@@ -11,7 +19,7 @@ objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
-base_input_path='./data/calibration_1024_1024/camera'+str(camera_no)+'/'
+base_input_path='/home/senthil/work/Camera_tracking/samples/11_30/lens_dist_inside/'+str(camera_no)+'/'
 images1 = glob.glob(base_input_path+'*.png')
 images2 = glob.glob(base_input_path+'*.jpg')
 images2 = glob.glob(base_input_path+'*.bmp')
@@ -60,7 +68,8 @@ data['intrinsic'] = mtx.tolist()
 data['dist'] = dist.tolist()
 # data['homography'] = Homography.tolist()
 # data['extrinsic'] = TransformMatrix.tolist()
-output_path = './config/camera_intrinsics_1024x1024/camera_'+str(camera_no)+'.json'
+output_path = output_base_path + 'camera_'+ str(camera_no) + '.json'
+
 with open(output_path, 'w') as f:
     json.dump(data, f)
 
